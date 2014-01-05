780177c5e95eb190cd111514998cd9823a2acbd9
Vasanthy Kolluri <vasanthykolluri@gmail.com>
2014-01-05 11:47:35 -0800

    Initial Commit

---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Vasanthy Kolluri on 1/5/14.
+//  Copyright (c) 2014 Vasanthy Kolluri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
